10e68168c7da3524cf8e8a50f96332fdb2bc83ad
Seohee-Kim <tjgml1343@gmail.com>
2024-08-15 15:17:02 +0900

    본문 2까지 변경

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# *** import ***
+# // import //
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_extras.metric_cards import style_metric_cards
@@ -9,17 +9,10 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import plotly.express as px
 import json
-# !pip install --force-reinstall --no-deps bokeh==2.4.3
-# from bokeh.plotting import figure
-# from bokeh.models import ColumnDataSource
-# from bokeh.palettes import Category20
-# from bokeh.models import HoverTool
 
 
-
-# *** 레이아웃 및 스타일 ***
+# // 레이아웃 및 스타일 //
 st.set_page_config(layout="wide")
-
 st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap');
@@ -53,9 +46,8 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-# *** 데이터베이스 민감정보 호출 ***
+# // 데이터베이스 민감정보 호출 //
 def get_dbjson(target):
-
     f  = open('db.json', encoding='UTF-8')
     dbjson = json.loads(f.read(),strict=False) 
     
@@ -68,18 +60,17 @@ def get_dbjson(target):
     return host, user, password, database, port
 
 
-#  *** 데이터 호출 ***
+#  // 데이터 호출 //
 def call_data(media_nm, target):
-    
     conn = pymysql.connect(host=get_dbjson(target)[0],
                            user=get_dbjson(target)[1],
                            password=get_dbjson(target)[2],
                            database=get_dbjson(target)[3],
                            port=get_dbjson(target)[4],
                            charset='utf8')
-    
     query = f"SELECT * FROM {media_nm}"
     rawdata = pd.read_sql(query, con=conn)
+    
     conn.close()
     
     # 전처리 (효율)
@@ -97,10 +88,11 @@ def call_data(media_nm, target):
     return rawdata
 
 
-# *** 메트릭 카드 ***
-def fig_metricCard(data, column_names): # 컬럼 이름을 리스트로 전달, 반복문을 사용하여 메트릭 카드를 동적으로 생성, 컬럼별 중앙값 계산
+# // 메트릭 카드 //
+# column_names = 리스트로 전달, 반복문을 사용하여 메트릭 카드를 동적으로 생성, 컬럼별 평균 계산
+def draw_metricCard(data, column_names): 
     cols = st.columns(len(column_names))
-
+    
     for i, col_name in enumerate(column_names):
         avg_value = data[col_name].mean()
         cols[i].metric(label=col_name, value=round(avg_value, 1), delta=0)
@@ -108,8 +100,8 @@ def fig_metricCard(data, column_names): # 컬럼 이름을 리스트로 전달, 
     style_metric_cards()
 
 
-# *** 파이 차트 ***
-def fig_pieChart(data, column_name, color_theme='Custom'):
+# // 파이 차트 //
+def draw_pieChart(data, column_name, color_theme='Custom'):
     
     # 수치형 또는 집계형
     if not pd.api.types.is_numeric_dtype(data[column_name]):
@@ -163,31 +155,27 @@ def fig_pieChart(data, column_name, color_theme='Custom'):
         annotations=[dict(text='', x=0.5, y=0.5, font_size=13, showarrow=False)])
 
     st.plotly_chart(fig, use_container_width=True)
-    
-
-# # *** 사이드 단일-셀렉박스 ***
-# def side_selectbox(data, colname, input_title):
-#     options = ['전체'] + data[colname].unique().tolist()
-#     result = st.sidebar.selectbox(input_title, options, index=0)
-#     return result
 
 
-# *** 사이드 체크박스 (복수 선택 가능) ***
-def side_checkbox(data, colname, input_title):
+# // 사이드 체크박스 - multiselect //
+def draw_sideCheckbox(data, colname, input_title):
     unique_options = data[colname].unique().tolist()
     selected_options = st.sidebar.multiselect(input_title, unique_options)
     return selected_options
 
 
-# *** 메인 함수 ***
-def main():
+# // 히트맵 //
+def create_heatmap(df, metric_columns, precision=2):
+    colors = ["#f6f5ff", "#BFB9FF"]
+    cmap_CustomMap = mcolors.LinearSegmentedColormap.from_list("CustomMap", colors) 
+    styled_df = df.style.background_gradient(cmap=cmap_CustomMap, subset=metric_columns).format(precision=precision)
+    return styled_df
 
-    # 하드코딩 요소
-    target_db = 'server2'
-    menu_names = ['메타', '구글', '메뉴3', '메뉴4']
-    menu_icons = ['facebook', 'google', 'heart', 'heart']
+
+# // 메인 //
+def main():
     
-    # 스타일
+    # 전체 스타일
     st.markdown("""
         <style>
         .css-1d391kg { padding: 1rem 1rem 1rem 1rem; }
@@ -202,133 +190,89 @@ def main():
         </style>
         """, unsafe_allow_html=True)
 
-    # 사이드바
+    # 전체 사이드바
     with st.sidebar:
-
-        st.header('the MAP 2.0')
-        st.markdown("더맵은 전사 광고 캠페인 데이터 기반 <br>디지털마케팅 벤치마크 플랫폼입니다", unsafe_allow_html=True)
-        st.text('')
-        menu = option_menu(None, menu_names, icons=menu_icons, menu_icon="cast", default_index=0)
+        # 정의
+        _sidebar_header = 'the MAP 2.0'
+        _sidebar_markdown = '더맵은 전사 광고 캠페인 데이터 기반 <br>디지털마케팅 벤치마크 플랫폼입니다'
+        _menu_names = ['메타', '구글', '메뉴3', '메뉴4'] # 리스트 순서대로 페이지 결정
+        _menu_icons = ['facebook', 'google', 'heart', 'heart']
+        # 구현
+        st.header(_sidebar_header)
+        st.markdown(_sidebar_markdown, unsafe_allow_html=True); st.text('')
+        menu = option_menu(None, _menu_names, icons=_menu_icons, menu_icon="cast", default_index=0)
     
     
-    ####################################################################################################################
-    ##############################################      1 페이지      ###################################################
-    ####################################################################################################################
-    
-    if menu == menu_names[0]:
+    # // 1페이지
+    if menu == _menu_names[0]:
         
-        # *** 데이터 ***
-        tb_meta = call_data('tb_meta', target_db)
-        temp_tb_meta = tb_meta.copy()        
-        temp_tb_meta = temp_tb_meta.fillna(0).replace([np.inf, -np.inf], 0)
+        # 데이터 - 전체
+        _data = call_data('tb_meta', 'server2')
+        temp_data = _data.copy()        
+        temp_data = temp_data.fillna(0).replace([np.inf, -np.inf], 0)
+        
+        # 데이터 - 기준 컬럼, 수치 컬럼 
+        _grouping_columns_options = ['media_name', 'adproduct_name', 'platform_position', 'device', 'bid_type', 'objective', 'buying_type', 'optimization_goal', 'billing_event', 'inds_name', 'year_month']
+        _metric_columns_options = ['CPM', 'CPC', 'CPV', 'CTR', 'VTR', 'clicks', 'impressions', 'reach', 'frequency', 'view_p25', 'view_p50', 'view_p75', 'view_p100', 'spend']
 
-        # # *** 사이드바 - 셀렉박스 영역 ***
-        # selected_media_name = side_selectbox(temp_tb_meta, 'media_name', '미디어 선택')
-        # selected_platform_position = side_selectbox(temp_tb_meta, 'platform_position', '게재위치 선택')
-        # selected_device = side_selectbox(temp_tb_meta, 'device', '기기 선택')
-        
-        # if selected_media_name != '전체':
-        #     temp_tb_meta = temp_tb_meta[temp_tb_meta['media_name'] == selected_media_name]
-        # if selected_platform_position != '전체':
-        #     temp_tb_meta = temp_tb_meta[temp_tb_meta['platform_position'] == selected_platform_position]
-        # if selected_device != '전체':
-        #     temp_tb_meta = temp_tb_meta[temp_tb_meta['device'] == selected_device]
-        
+        # 사이드 체크박스
+        _filter_conditions = {
+            'year_month': '날짜 선택',
+            'inds_name': '업종 선택',
+            'media_name': '미디어 선택',
+            'adproduct_name': '광고상품 선택',
+            'platform_position': '게재위치 선택',
+            'device': '기기 선택'}
+        selected_filters = {col: draw_sideCheckbox(temp_data, col, label) for col, label in _filter_conditions.items()} # 사이드바에서 선택된 필터 값을 저장할 딕셔너리
+        for col, selected_values in selected_filters.items(): # 선택된 필터가 있는 경우 해당 데이터를 필터링
+            if selected_values:
+                temp_data = temp_data[temp_data[col].isin(selected_values)]
 
-        # *** 사이드바 - 체크박스 영역 ***
-        selected_year_months = side_checkbox(temp_tb_meta, 'year_month', '날짜 선택')
-        selected_inds_names = side_checkbox(temp_tb_meta, 'inds_name', '업종 선택')
-        selected_media_names = side_checkbox(temp_tb_meta, 'media_name', '미디어 선택')
-        selected_adproduct_names = side_checkbox(temp_tb_meta, 'adproduct_name', '광고상품 선택')
-        selected_platform_positions = side_checkbox(temp_tb_meta, 'platform_position', '게재위치 선택')
-        selected_devices = side_checkbox(temp_tb_meta, 'device', '기기 선택')
-        
-        # 사이드바 필터 즉시 적용
-        if selected_year_months:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['year_month'].isin(selected_year_months)]
-        if selected_inds_names:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['inds_name'].isin(selected_inds_names)]
-        if selected_media_names:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['media_name'].isin(selected_media_names)]
-        if selected_adproduct_names:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['adproduct_name'].isin(selected_adproduct_names)]
-        if selected_platform_positions:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['platform_position'].isin(selected_platform_positions)]
-        if selected_devices:
-            temp_tb_meta = temp_tb_meta[temp_tb_meta['device'].isin(selected_devices)]
-        
-        # *** 본문 ***
+        # 본문 0 - 제목
         st.header("메타 대시보드", divider='gray')
         
-        # 본문 1 - 메트릭 카드 영역
+        # 본문 1 - 메트릭 카드
         st.text(" ")
-        fig_metricCard(temp_tb_meta, ['CPM', 'CPC', 'CPV', 'CTR', 'VTR'])
+        draw_metricCard(temp_data, ['CPM', 'CPC', 'CPV', 'CTR', 'VTR'])
+    
+        # 본문 2 - 효울 확인
+        st.header(" ", anchor=False)
+        st.subheader("효율 확인")
+        st.markdown('선택한 기준 컬럼에 대한 평균 효율 및 수치를 확인할 수 있습니다.')
         
-        # 본문 2 - 테이블
-        st.subheader(" ")
-        st.subheader("기준별 효율 검색")
-        st.markdown('효율을 확인해보세요. 선택하는 "기준 컬럼"에 따라서 데이터가 집계됩니다.')
-        
-        # 기준 컬럼 (grouping_columns)
-        grouping_columns_options = ['media_name', 'adproduct_name', 'platform_position', 'device', 'bid_type', 'objective', 'buying_type', 'optimization_goal', 'billing_event', 'inds_name', 'year_month']
-        grouping_columns_default = ['media_name', 'objective']
-        
-        # 수치 컬럼 (metric_columns)
-        metric_columns_options = ['CPM', 'CPC', 'CPV', 'CTR', 'VTR', 'clicks', 'impressions', 'reach', 'frequency', 'view_p25', 'view_p50', 'view_p75', 'view_p100', 'spend']
-        metric_columns_default = ['CPM', 'CTR', 'impressions', 'clicks', 'spend']
-
-        # form holding
-        with st.form(key='my_form'):
-            col1, col2, col3 = st.columns([6, 6, 1])  # 비율
-            
+        # 본문 2 - my_form1
+        with st.form(key='my_form1'):
+            _grouping_columns_options_default = ['media_name']
+            _metric_columns_options_default = ['CPM', 'CTR', 'impressions', 'clicks', 'spend']
+            col1, col2, col3 = st.columns([6, 6, 1])
             with col1:
-                grouping_columns = st.multiselect("기준 컬럼을 선택하세요.", options= grouping_columns_options, default=grouping_columns_default)
-            
+                grouping_columns = st.multiselect("기준 컬럼을 선택하세요.", options= _grouping_columns_options, default=_grouping_columns_options_default)
             with col2:
-                metric_columns = st.multiselect("수치 컬럼을 선택하세요.", options=metric_columns_options, default=metric_columns_default)
-
+                metric_columns = st.multiselect("수치 컬럼을 선택하세요.", options=_metric_columns_options, default=_metric_columns_options_default)
             with col3:
-                # st.write("")  # 버튼을 아래로 내리기 위한 빈 줄
-                st.markdown("<div style='height: 27px;'></div>", unsafe_allow_html=True)  # 30px 만큼의 공간을 추가
-                submit_button = st.form_submit_button(label='적용')  # 버튼을 오른쪽에 배치
-
-        # # 디폴트 데이터프레임
-        # grouped_df = temp_tb_meta.groupby(grouping_columns_default)[metric_columns_default].mean().reset_index()
-        # dataframe_placeholder = st.dataframe(grouped_df.head(100).style.hide(axis='index'), use_container_width=True)
-        
-        # # 사용자가 "적용" 버튼을 눌렀을 때만 데이터프레임을 업데이트
-        # if submit_button:
-        #     if grouping_columns and metric_columns:
-        #         grouped_df = temp_tb_meta.groupby(grouping_columns)[metric_columns].mean().reset_index()
-        #         dataframe_placeholder.dataframe(grouped_df.head(100).style.hide(axis='index'), use_container_width=True)
-        #     else:
-        #         st.write("기준 컬럼과 수치 컬럼을 선택하세요.")
+                st.markdown("<div style='height: 27px;'></div>", unsafe_allow_html=True)
+                submit_button = st.form_submit_button(label='적용')
                 
-        # 디폴트 데이터프레임
-        grouped_df = temp_tb_meta.groupby(grouping_columns_default)[metric_columns_default].mean().fillna(0).reset_index() #fillna
-        grouped_df = grouped_df.replace([np.inf, -np.inf], 0)
+        # 본문 2 - 데이터프레임 (디폴트 데이터프레임으로 시작)
+        if grouping_columns and metric_columns:
+            grouped_df = temp_data.groupby(_grouping_columns_options_default)[_metric_columns_options_default].mean().fillna(0).reset_index()
+            grouped_df = grouped_df.replace([np.inf, -np.inf], 0)
+            styled_df = create_heatmap(grouped_df.head(100), _metric_columns_options_default) # 히트맵 적용
+            dataframe_placeholder = st.dataframe(styled_df.hide(axis='index'), use_container_width=True) # 인덱스 숨기기
+        else:
+            pass # 패스, 다음 else문에서 워닝
 
-        # 히트맵
-        # 원하는 색상으로 히트맵을 만들기 위한 컬러 맵 생성
-        colors = ["#f6f5ff", "#BFB9FF"]
-        
-        cmap_CustomMap = mcolors.LinearSegmentedColormap.from_list("CustomMap", colors)
-        
-        styled_df = grouped_df.head(100).style.background_gradient(cmap=cmap_CustomMap, subset=metric_columns_default).format(precision=2)
-        dataframe_placeholder = st.dataframe(styled_df.hide(axis='index'), use_container_width=True)
-
-        # 사용자가 "적용" 버튼을 눌렀을 때만 데이터프레임을 업데이트
+        # 본문 2 - 데이터프레임 업데이트 (버튼 클릭으로 변경)
         if submit_button:
             if grouping_columns and metric_columns:
-                grouped_df = temp_tb_meta.groupby(grouping_columns)[metric_columns].mean().reset_index()
-                styled_df = grouped_df.head(100).style.background_gradient(cmap=cmap_CustomMap, subset=metric_columns).format(precision=2)
+                grouped_df = temp_data.groupby(grouping_columns)[metric_columns].mean().reset_index()
+                styled_df = create_heatmap(grouped_df.head(100), metric_columns) # 히트맵 적용
                 dataframe_placeholder.dataframe(styled_df.hide(axis='index'), use_container_width=True)
             else:
-                st.write("기준 컬럼과 수치 컬럼을 선택하세요.")
-        
-        
+                st.warning("기준 컬럼과 수치 컬럼을 선택해주세요. 선택된 컬럼이 없을 경우 데이터를 표시할 수 없습니다.")
+            
         # 본문 3 - 차트
-        st.subheader(" ")
+        st.header(" ", anchor=False)
         st.subheader("기준별 비중 확인")
 
         # col1, col2 = st.columns(2)
@@ -336,16 +280,16 @@ def main():
         
         # with col1:
         #     st.write("미디어 집행비중")
-        #     fig_pieChart(temp_tb_meta, 'media_name', color_theme='Custom')
+        #     draw_pieChart(temp_data, 'media_name', color_theme='Custom')
         # with col2:
         #     st.write("목적 집행비중")
-        #     fig_pieChart(temp_tb_meta, 'objective', color_theme='Custom')
+        #     draw_pieChart(temp_data, 'objective', color_theme='Custom')
         # with col3:
         #     st.write("기기 집행비중")
-        #     fig_pieChart(temp_tb_meta, 'device', color_theme='Custom')
+        #     draw_pieChart(temp_data, 'device', color_theme='Custom')
         # with col4:
         #     st.write("업종 집행비중")
-        #     fig_pieChart(temp_tb_meta, 'inds_name', color_theme='Custom')
+        #     draw_pieChart(temp_data, 'inds_name', color_theme='Custom')
         
 
         # 기준 컬럼 (grouping_columns)
@@ -366,7 +310,7 @@ def main():
 
         # 데이터프레임 초기화 및 조건에 따른 처리
         if ratio_grouping_columns:
-            grouped_df_ratio = temp_tb_meta.groupby(ratio_grouping_columns).size().reset_index(name='count')
+            grouped_df_ratio = temp_data.groupby(ratio_grouping_columns).size().reset_index(name='count')
             grouped_df_ratio['percentage'] = grouped_df_ratio['count'] / grouped_df_ratio['count'].sum() * 100
 
             # **변경**: percentage 기준으로 내림차순 정렬
@@ -380,7 +324,7 @@ def main():
         # 사용자가 "적용" 버튼을 눌렀을 때만 데이터프레임을 업데이트
         if submit_button_ratio:
             if ratio_grouping_columns:
-                grouped_df_ratio = temp_tb_meta.groupby(ratio_grouping_columns).size().reset_index(name='count')
+                grouped_df_ratio = temp_data.groupby(ratio_grouping_columns).size().reset_index(name='count')
                 grouped_df_ratio['percentage'] = grouped_df_ratio['count'] / grouped_df_ratio['count'].sum() * 100
                 
                 # **변경**: percentage 기준으로 내림차순 정렬
@@ -390,7 +334,7 @@ def main():
                 dataframe_ratio_placeholder.dataframe(styled_df_ratio, use_container_width=True)
             else:
                 # **변경**: ratio_grouping_columns가 비어 있을 때 경고 메시지 표시
-                st.warning("기준 컬럼을 선택해주세요. 선택된 기준 컬럼이 없을 경우 데이터를 표시할 수 없습니다.")
+                st.warning("기준 컬럼을 선택해주세요. 선택된 컬럼이 없을 경우 데이터를 표시할 수 없습니다.")
 
 
         
@@ -401,7 +345,7 @@ def main():
 
 
         # 본문 4 - 그래프 / Bokeh 대신 Plotly를 사용
-        st.subheader(" ")
+        st.header(" ", anchor=False)
         st.subheader("효율 추이")
         
         # form holding
@@ -409,7 +353,7 @@ def main():
             col1, col2 = st.columns([12, 1])  # 비율
             
             with col1:
-                metric_columns = st.multiselect("수치 컬럼을 선택하세요.", options=metric_columns_options, default=['CPM'])
+                metric_columns = st.multiselect("수치 컬럼을 선택하세요.", options=_metric_columns_options, default=['CPM'])
 
             with col2:
                 # st.write("")  # 버튼을 아래로 내리기 위한 빈 줄
@@ -417,7 +361,7 @@ def main():
                 submit_button = st.form_submit_button(label='적용')  # 버튼을 오른쪽에 배치
 
         # 디폴트 그래프
-        grouped_data = temp_tb_meta.groupby('year_month')['CPM'].mean().reset_index()
+        grouped_data = temp_data.groupby('year_month')['CPM'].mean().reset_index()
         grouped_data = grouped_data.sort_values('year_month')
         
         fig = px.line(grouped_data, x='year_month', y=['CPM'], labels={'year_month': '연도_월', 'value': '값'})
@@ -439,7 +383,7 @@ def main():
         # 사용자가 "적용" 버튼을 눌렀을 때만 그래프를 업데이트
         if submit_button:
             if metric_columns:
-                grouped_data = temp_tb_meta.groupby('year_month')[metric_columns].mean().reset_index()
+                grouped_data = temp_data.groupby('year_month')[metric_columns].mean().reset_index()
                 grouped_data = grouped_data.sort_values('year_month')
 
                 # Plotly 시각화
